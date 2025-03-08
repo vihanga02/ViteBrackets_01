e21478d52b9e234dd91e22b1e04b5947f38abc9c
rvn14 <dasunadithya123@gmail.com>
2025-03-08 18:36:13 +0530

    LoginUI

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar: React.FC<NavbarProps> = ({ isSigned }) => {
   return (
     <div ref={navContainerRef} className='fixed inset-x-2 top-4 z-50 h-16 sm:inset-x-6 rounded-4xl from-gray-900/40 to-gray-600/60 bg-gradient-to-r backdrop-blur-3xl shadow-lg border border-white/20 '>
         <div className='flex justify-between items-center h-full px-8'>
-            <div className='text-white font-geo font-medium text-2xl'>SecureConnect.</div>
+            <div className='text-white font-geo font-medium text-2xl'>SecureConnect</div>
             <div className='flex items-center space-x-4'>
                 {isSigned? <Button id='login' title='Sign up' containerClass='text-white' /> : <Button id='login' title='Sign up' containerClass='text-white' />}
 
